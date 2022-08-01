227b3c3efbcffae0c8e7c419f838005357c2dca4
abdulazizzakihidayat <zakihidayat260@gmail.com>
2022-08-01 10:53:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/customfancontroller/DialView.kt
@@ -0,0 +1,94 @@
+package com.example.customfancontroller
+
+import android.content.Context
+import android.graphics.*
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import java.time.format.TextStyle
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
+
+private enum class FanSpeed(val label : Int){
+    OFF(R.string.fan_off),
+    LOW(R.string.fan_low),
+    MEDIUM(R.string.fan_medium),
+    HIGH(R.string.fan_high);
+
+    fun next() = when (this){
+        OFF -> LOW
+        LOW -> MEDIUM
+        MEDIUM -> HIGH
+        HIGH -> OFF
+    }
+}
+
+private const val RADIUS_OFFSET_LABEL = 30
+private const val RADIUS_OFFSET_INDICATOR = -35
+
+class DialView @JvmOverloads constructor(
+    context: Context,
+    attribut: AttributeSet? = null,
+    defStylAttributeSet: Int = 0
+) : View (context, attribut, defStylAttributeSet) {
+    private var radius = 0.0f
+    private var fanSpeed = FanSpeed.OFF
+
+    private val pointPosition : PointF = PointF(0.0f, 0.0f)
+
+    private val paint =  Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
+
+    init {
+        isClickable = true
+    }
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        fanSpeed = fanSpeed.next()
+        contentDescription = resources.getString(fanSpeed.label)
+
+        invalidate()
+        return true
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = (min(w,h) / 2.0 * 0.8 ).toFloat()
+    }
+
+    private fun PointF.computedXYForSpeed(pos : FanSpeed, radius : Float){
+        val startAngle = Math.PI * (9 / 8.0)
+        val angle = startAngle + pos.ordinal * (Math.PI / 4)
+        x = (radius * cos(angle)).toFloat() + width / 2
+        y = (radius * sin(angle)).toFloat() + height / 2
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+        canvas?.drawCircle((width/2).toFloat(), (height / 2).toFloat(), radius, paint)
+
+        val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+        pointPosition. computedXYForSpeed(fanSpeed, markerRadius)
+        paint.color = Color.BLACK
+        canvas?.drawCircle(pointPosition.x, pointPosition.y, radius/12, paint)
+
+        //draw label
+        val labelRadius = radius + RADIUS_OFFSET_LABEL
+        for (i in FanSpeed.values()) {
+            pointPosition.computedXYForSpeed(i, labelRadius)
+            val label = resources.getString(i.label)
+            canvas?.drawText(label, pointPosition.x, pointPosition.y, paint)
+        }
+    }
+
+
+
+}
